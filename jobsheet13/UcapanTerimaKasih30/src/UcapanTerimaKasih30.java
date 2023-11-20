69e3a76e7edade2a111a519ae46b7b0abf99ddef
viraalfita <viraalfita1813@gmail.com>
2023-11-20 13:39:03 +0700

    jobsheet 13 percobaan 4

---
+++ jobsheet13/UcapanTerimaKasih30/src/UcapanTerimaKasih30.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UcapanTerimaKasih30 {
+    public static void main(String[] args) throws Exception {
+        UcapanTerimaKasih();
+    }
+
+    public static String PenerimaUcapan() {
+        Scanner scan30 = new Scanner(System.in);
+        System.out.print("Tuliskan nama orang yang ingin anda berikan ucapan : ");
+        String nama = scan30.nextLine();
+        scan30.close();
+        return nama;
+    }
+
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama
+                + "for being the best teacher in the world \nYou inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+}
