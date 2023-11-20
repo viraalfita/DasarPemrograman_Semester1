145f349a3d09dd57b1eeeac985e82e172f1e1e59
viraalfita <viraalfita1813@gmail.com>
2023-11-20 12:40:08 +0700

    jobsheet 13 percobaan 1

---
+++ jobsheet12/TerimaKasih30/src/TerimaKasih30.java
@@ -0,0 +1,10 @@
+public class TerimaKasih30 {
+    public static void main(String[] args) throws Exception {
+        UcapanTerimaKasih();
+    }
+
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"
+                + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+}
