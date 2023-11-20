b4e9a5373c80fe954565e2d3665267121709c6ed
viraalfita <viraalfita1813@gmail.com>
2023-11-20 14:25:23 +0700

    modifikasi pertanyaan 3

--- jobsheet13/UcapanTerimaKasih30/src/UcapanTerimaKasih30.java
+++ jobsheet13/UcapanTerimaKasih30/src/UcapanTerimaKasih30.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class UcapanTerimaKasih30 {
     public static void main(String[] args) throws Exception {
         UcapanTerimaKasih();
+        UcapanTambahan("May Allah bless you");
     }
 
     public static String PenerimaUcapan() {
@@ -17,5 +18,9 @@ public class UcapanTerimaKasih30 {
         String nama = PenerimaUcapan();
         System.out.println("Thank you " + nama
                 + "for being the best teacher in the world \nYou inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapanTambahan) {
+        System.out.println(ucapanTambahan);
     }
 }
