ebf066b6f613d042e3740e378e64cdd793ffc980
viraalfita <viraalfita1813@gmail.com>
2023-11-27 13:40:23 +0700

    modifikasi pertanyaan 2

--- jobsheet14/Rekursif30/src/Percobaan230.java
+++ jobsheet14/Rekursif30/src/Percobaan230.java
@@ -9,16 +9,28 @@ public class Percobaan230 {
         bilangan = scan30.nextInt();
         System.out.print("Pangkat                : ");
         pangkat = scan30.nextInt();
-        System.out.println("Hasil                  : " + hitungPangkat(bilangan, pangkat));
+        System.out.print("Hasil                  : " + hitungPangkat(bilangan, pangkat));
+
+        System.out.print(" = ");
+        printPangkat(bilangan, pangkat);
 
         scan30.close();
     }
 
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
-            return (x * hitungPangkat(x, y - 1));
+            return x * hitungPangkat(x, y - 1);
+        }
+    }
+
+    static void printPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.print("1");
+        } else {
+            System.out.print(x + "x");
+            printPangkat(x, y - 1);
         }
     }
 }
